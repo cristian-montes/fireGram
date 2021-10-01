1d51a2607e62770d72fc3c7e77870522b6761eb5
Cristian Montes <cmontesmujica@gmail.com>
2021-10-01 15:08:52 -0700

    added and installed firebase to FE

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+//Your web app's Firebase Configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBnXnAc--z3ArgOBSkc8WIII8i6uHFwD3g",
+    authDomain: "cristian-firegram.firebaseapp.com",
+    projectId: "cristian-firegram",
+    storageBucket: "cristian-firegram.appspot.com",
+    messagingSenderId: "551403478184",
+    appId: "1:551403478184:web:7c176538472ce6e915b69d"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
