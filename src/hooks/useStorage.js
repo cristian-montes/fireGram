7d5866715a155d78f8ce4c1301860858b44c93f5
Cristian Montes <cmontesmujica@gmail.com>
2021-10-12 21:20:56 -0700

    added the collectionfer to added url and time stamp to the files uploaded

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -1,6 +1,6 @@
 //This hook handles file uploads and file storafes to the firebase storage
 import { useState, useEffect } from "react";
-import { projectStorage } from '../firebase/config';
+import { projectStorage , projectFirestore, timestamp} from '../firebase/config';
 
 const useStorage = (file) => {
     const [progress, setProgress ] = useState(0);
@@ -11,6 +11,8 @@ const useStorage = (file) => {
         //references
         const storageRef = projectStorage.ref(file.name); //**** do more research about the .ref(), .put() and .on() ****
 
+        const collectionRef = projectFirestore.collection('images');
+
         storageRef.put(file).on('state_changed', (snap) => {
             let percentage = (snap.bytesTransferred/ snap.totalBytes) * 100;
             setProgress(percentage);
@@ -18,6 +20,8 @@ const useStorage = (file) => {
             setError(err);
         }, async () => {
             const url = await storageRef.getDownloadURL();
+            const createdAt =  timestamp();
+            collectionRef.add({ url, createdAt })
             setUrl(url);
         })
     }, [file]);
