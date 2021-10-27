5f27b8edc91b4824aec7d301b56a1329ac7e028e
Cristian Montes <cmontesmujica@gmail.com>
2021-10-26 20:49:59 -0600

    framer motion working on Modal to display pic from top to bottom

--- src/comps/Modal.js
+++ src/comps/Modal.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const Modal = ({selectImg, setSelectedImg}) => {
     const handleClick = (e) => {
@@ -7,9 +8,15 @@ const Modal = ({selectImg, setSelectedImg}) => {
         }
     }
     return(
-        <div className ="backdrop" onClick={handleClick} >
-            <img src={selectImg} alt="enlarged pic" />
-        </div>
+        <motion.div className ="backdrop" onClick={handleClick}
+            initial={{ opacity:0 }}
+            animate={{ opacity: 1 }}
+        >
+            <motion.img src={selectImg} alt="enlarged pic" 
+                initial={{y: '-100vh'}}
+                animate={{ y: 0 }}
+            />
+        </motion.div>
     )
 }
 
